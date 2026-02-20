8c1e1d66af4c5d426dbea4b5a78a70c267c15637
BigFishay <gamebrycew@gmail.com>
2026-02-19 22:56:54 -0700

    Added License and header to the top of the program file.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿using Zxcvbn;
+﻿/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+/*
+ * Written by GitHub User BigFishay
+ */
 
 class Program
 {
